39507d3a92c0cff80bf96fe4f34d1aeea7755756
pawelbalcerek <pawbal96@gmail.com>
2026-03-08 18:11:59 +0100

    fix lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
 	{ "williamboman/mason.nvim", opts = {} },
 	{
 		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",

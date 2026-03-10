ed12144ea4d1eb637f7af9080cc95bca39a3c9a9
PawelBalcerek <pawel.balcerek@allegro.com>
2026-03-10 10:55:16 +0100

    identetation settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,12 @@ vim.opt.compatible = false
 vim.opt.autoread = false
 vim.g["test#strategy"] = "dispatch"
 
+-- Tab/indentation configuration
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
 -- Line length configuration
 vim.opt.textwidth = 120
 vim.opt.colorcolumn = "120"

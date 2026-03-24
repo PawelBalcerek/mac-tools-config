fe56752eae7722b4dfc67e6d70ddd15590aa3164
PawelBalcerek <pawel.balcerek@allegro.com>
2026-03-24 09:24:52 +0100

    align main with copiot branch

--- nvim/lua/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"github/copilot.vim",
-	event = "BufEnter",
-	keys = {
-		{ "<leader>ce", ":let g:copilot_enabled = 1<CR>", desc = "Enable Copilot" },
-		{ "<leader>cd", ":let g:copilot_enabled = 0<CR>", desc = "Disable Copilot" },
-	},
-}

10d0cb31252cbee16088f6e71013558d858b8a3f
pawelbalcerek <pawbal96@gmail.com>
2026-03-24 17:02:35 +0100

    add codeium

---
+++ nvim/lua/plugins/codeium.lua
@@ -0,0 +1,8 @@
+return {
+	"Exafunction/codeium.vim",
+	event = "BufEnter",
+	keys = {
+		{ "<leader>ce", ":Codeium Enable<CR>", desc = "Enable codeium" },
+		{ "<leader>cd", ":Codeium Disable<CR>", desc = "Disable codeium" },
+	},
+}
